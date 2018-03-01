757483adae175b4e05aa8ecca1b28b692eb85319
DESKTOP-BD4IU5S\Thithirat <thithirat.chsw@gmail.com>
2018-03-01 23:44:26 +0700

    can use drop down without spinner (use button)

--- app/src/main/java/com/example/thithirat/test/AddLocationFragment.java
+++ app/src/main/java/com/example/thithirat/test/AddLocationFragment.java
@@ -1,6 +1,8 @@
 package com.example.thithirat.test;
 
 
+import android.animation.LayoutTransition;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,7 +10,12 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.PopupMenu;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -17,19 +24,19 @@ import android.widget.Toast;
  */
 public class AddLocationFragment extends Fragment {
 
-
     public AddLocationFragment() {
         // Required empty public constructor
     }
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_add_location, container, false);
+        final View view = inflater.inflate(R.layout.fragment_add_location, container, false);
 
-        Button button_type_location = (Button)view.findViewById(R.id.button_type_location);
+        final Button button_type_location = (Button)view.findViewById(R.id.button_type_location);
+
 
         button_type_location.setOnClickListener(new View.OnClickListener() {
 
@@ -45,10 +52,10 @@ public class AddLocationFragment extends Fragment {
                         return false;
                     }
                 });
-
                 popupMenu.show();
             }
         });
+
 
         return view;
     }
