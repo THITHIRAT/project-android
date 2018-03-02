e8281882cf53906e4f0a7e368ec0ad08edbe8a54
DESKTOP-BD4IU5S\Thithirat <thithirat.chsw@gmail.com>
2018-03-02 15:59:20 +0700

    can use drop down with spinner and add floating button in add reminder location fragment and edit location fragment but can action for add reminder location only

--- app/src/main/java/com/example/thithirat/test/AddLocationFragment.java
+++ app/src/main/java/com/example/thithirat/test/AddLocationFragment.java
@@ -4,17 +4,22 @@ package com.example.thithirat.test;
 import android.animation.LayoutTransition;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.PopupMenu;
 import android.widget.RelativeLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,6 +28,9 @@ import android.widget.Toast;
  * A simple {@link Fragment} subclass.
  */
 public class AddLocationFragment extends Fragment {
+
+    Spinner spinner;
+    ArrayAdapter<String> adapter;
 
     public AddLocationFragment() {
         // Required empty public constructor
@@ -35,27 +43,24 @@ public class AddLocationFragment extends Fragment {
         // Inflate the layout for this fragment
         final View view = inflater.inflate(R.layout.fragment_add_location, container, false);
 
-        final Button button_type_location = (Button)view.findViewById(R.id.button_type_location);
+        spinner = (Spinner)view.findViewById(R.id.spinner_location);
+        adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.types_location_notification));
 
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
-        button_type_location.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fab_done_reminder = (FloatingActionButton)view.findViewById(R.id.fab_done);
+        fab_done_reminder.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
-                PopupMenu popupMenu = new PopupMenu(getActivity().getApplicationContext(), v);
-                popupMenu.inflate(R.menu.popup_type_location);
-
-                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                    @Override
-                    public boolean onMenuItemClick(MenuItem item) {
-                        Toast.makeText(getActivity().getApplicationContext(), "" + item.getTitle(), Toast.LENGTH_SHORT).show();
-                        return false;
-                    }
-                });
-                popupMenu.show();
+                ScheduledFragment scheduled_fragment = new ScheduledFragment();
+                FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.frag, scheduled_fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
             }
         });
-
 
         return view;
     }
